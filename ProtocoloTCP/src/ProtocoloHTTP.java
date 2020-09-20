63cd878aaa57764e1b3922bb7499681c483fe870
JU4NG1R4L <juanj8845@gmail.com>
2020-09-20 16:34:14 -0500

    Cambios headers

--- ProtocoloTCP/src/ProtocoloHTTP.java
+++ ProtocoloTCP/src/ProtocoloHTTP.java
@@ -62,29 +62,60 @@ public class ProtocoloHTTP implements Runnable{
       }
 	  
 	  salida.write(("HTTP/1.1 \r\n" + status).getBytes());
-      System.out.println("HTTP/1.1 " + status);
+      System.out.println("HTTP/1.1 " + status);      
+      
+      /*----------------TIPOS DE CACHE-------------------*/
+      
+//      /*El caché no debe almacenar nada sobre la solicitud del cliente o la respuesta del servidor. 
+//       * Se envía una solicitud al servidor y se descarga una respuesta completa cada vez.*/
+//      salida.write(("Cache-Control: no-store").getBytes());
+//      System.out.println("Cache-control: no-store");
+//
+//      /*El caché no debe almacenar nada sobre la solicitud del cliente o la respuesta del servidor. 
+//       * Se envía una solicitud al servidor y se descarga una respuesta completa cada vez.*/
+//      salida.write(("Cache-Control: no-cache, no-store, must-revalidate").getBytes());
+//      System.out.println("Cache-control: no-cache, no-store, must-revalidate");
+//      
+//	  /*Por otro lado, la directiva "private" indica que la respuesta está dirigida a un solo usuario y no debe ser 
+//	  almacenada por un caché compartido. Un caché de navegador privado puede almacenar la respuesta en este caso.*/
+//      salida.write(("Cache-Control: private").getBytes());
+//      System.out.println("Cache-control: private");
+//      
+//      /*La directiva "public" indica que la respuesta puede ser almacenada en caché por cualquier caché. 
+//       *Esto puede ser útil, si las páginas con autentificación HTTP o códigos de estado de respuesta que normalmente 
+//       *no se pueden almacenar en caché, ahora deben almacenarse en caché.*/
+//      salida.write(("Cache-Control: public").getBytes());
+//      System.out.println("Cache-control: public");
+      
+      /*La directiva más importante aquí es "max-age=<seconds>" que es la máxima cantidad de tiempo que un recurso 
+       * será considerado nuevo. Contrariamente a Expires, esta directiva es relativa al momento de la solicitud. 
+       * Para los archivos de la aplicación que no cambiarán, generalmente se puede agregar almacenamiento en caché 
+       * agresivo.Esto incluye archivos estáticos como imágenes, archivos CSS y archivos JavaScrip*/
+      salida.write(("Cache-Control: max-age=3600").getBytes());
+      //System.out.println("Cache-control: max-age=3600");
+      
+      /*----------------TIPOS DE CACHE-------------------*/
       
       salida.write(("Content-Length: " + content_lenght + "\r\n").getBytes());
-      System.out.println("Content-Length: " + content_lenght);
+      //System.out.println("Content-Length: " + content_lenght);
       
       salida.write(("ContentType: " + contentType + "\r\n").getBytes());
-      System.out.println("ContentType: " + contentType);
+      //System.out.println("ContentType: " + contentType);
       
       salida.write("\r\n".getBytes());
-      System.out.println("\r\n");
+      //System.out.println("\r\n");
    
       if(existe) {
     	  while((bytesRead = contenido.read(bloque))==BLOCK_SIZE){
               salida.write(bloque, 0, bytesRead);
-              System.out.println(new String(bloque));
+              //System.out.println(new String(bloque));
           } 
       }else {
     	  salida.write(bloque);
       }
       
-      
       salida.write("\r\n\r\n".getBytes());
-      System.out.println("\r\n\r\n");
+      //System.out.println("\r\n\r\n");
       
       salida.flush();
       client.close();
--- ProtocoloTCP/src/Servidor.java
+++ ProtocoloTCP/src/Servidor.java
@@ -5,10 +5,14 @@ public class Servidor {
 
 	 public static void main( String[] args ) throws Exception {
 	    	try (ServerSocket serverSocket = new ServerSocket(45000)) {
+
 	            while (true) {
+		    		long t1 = System.nanoTime();
 	            	Socket client = serverSocket.accept();
 	                ProtocoloHTTP hilo = new ProtocoloHTTP(client);
 	                new Thread(hilo).start();
+			        long t = System.nanoTime() - t1;
+			        System.out.println("t(ms) = "+t/1000000 + " ms");
 	            }
 	        }
 	    }
